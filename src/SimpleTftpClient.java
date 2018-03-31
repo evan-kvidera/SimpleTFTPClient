9f45fe308755c974344e33ea3f616276bf0a67b8
Evan Kvidera <ekvidera15@winona.edu>
2018-03-31 15:55:17 -0500

    Preliminary get <file> testing

--- src/SimpleTftpClient.java
+++ src/SimpleTftpClient.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
@@ -7,27 +9,34 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class SimpleTftpClient {
 
-	InetAddress hostAddress;
-	ByteBuffer bb;
+	InetAddress hostAddress = null;
+	int port = 69;
+	ByteBuffer sendBuffer;
+	ByteBuffer recvBuffer;
 	DatagramSocket sock;
-	DatagramPacket packet;
+	DatagramPacket sendPacket;
+	DatagramPacket recvPacket;
+	Mode mode = Mode.BINARY;
 
 	public SimpleTftpClient() {
-		bb = ByteBuffer.allocate(516);
-
+		sendBuffer = ByteBuffer.allocate(516);
+		recvBuffer = ByteBuffer.allocate(516);
 		try {
 			sock = new DatagramSocket();
 		} catch (SocketException se) {
 			System.err.println("Socket error: could not create datagram socket");
 			System.exit(1);
 		}
-		packet = new DatagramPacket(bb.array(), 0);	
+		sendPacket = new DatagramPacket(sendBuffer.array(), 0);
+		recvPacket = new DatagramPacket(recvBuffer.array(), 0);
 	}
 
-	public static void main() {
+	public static void main(String[] args) {
 		SimpleTftpClient client = new SimpleTftpClient();
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		String line;
@@ -56,37 +65,69 @@ public class SimpleTftpClient {
 		String[] cmdArgs = command.split(" ");
 		switch (cmdArgs[0]) {
 		case "connect":
-			break;
+			return null;
 		case "get":
 			if (cmdArgs.length != 2) {
 				return "invalid number of arguments: " + (cmdArgs.length - 1) 
 						+ "\nusage: get filename";
 			} else {
-				return getFile(cmdArgs[1]);
+				return receiveFile(cmdArgs[1]);
 			}
-			break;
 		case "put":
 			if (cmdArgs.length != 2) {
 				return "invalid number of arguments: " + (cmdArgs.length - 1) 
 						+ "\nusage: get filename";
 			}
-			break;
+			return null;
 		case "mode":
-			break;
+			return null;
 		case "help":
-			break;
+			return null;
 		case "quit":
 			System.exit(0);
-			break;
+			return null;
 		default:
 			return "unrecognized command: " + cmdArgs[0];
 		}
-		}
 	}
 
-	public String getFile(String filename) {
+	public String receiveFile(String filename) {
+		if (hostAddress == null) return "unable to receive file: host not specified";
 		
+		if (mode == Mode.BINARY) {
+			sendBuffer.clear();
+			sendBuffer.put(filename.getBytes(StandardCharsets.US_ASCII));
+			sendBuffer.put((byte) 0);
+			sendBuffer.put("octet".getBytes(StandardCharsets.US_ASCII));
+			sendBuffer.put((byte) 0);
+			
+			sendPacket.setLength(sendBuffer.position());
+			sendPacket.setAddress(hostAddress);
+			sendPacket.setPort(port);
+			
+			try {
+				sock.send(sendPacket);
+				
+				recvBuffer.clear();
+				recvPacket.setLength(516);
+				
+				sock.receive(recvPacket);
+				System.out.println(Arrays.toString(recvPacket.getData()));
+				return ("we gotta packet boiiiisss");
+				
+			} catch (IOException e) {
+				return "IO error: " + e.getMessage();
+			}
+			
+			
+			
+		}
+		
+		
+		return null;
 	}
-
-
+	
+	public enum Mode {
+		ASCII, BINARY
+	}
 }
